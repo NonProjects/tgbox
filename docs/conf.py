a6fc368c869afa0a8d3421496ec6be885f78521d
NotStatilko <valerastatilko@gmail.com>
2021-12-14 02:11:10 +0200

    Fix broken tgbox docs in RTD

--- docs/conf.py
+++ docs/conf.py
@@ -13,11 +13,7 @@
 import os
 import sys
 
-sys.path.insert(0, [
-    os.path.abspath('.'),
-    os.path.abspath('../..'),
-    os.path.abspath('../../../'), # ReadTheDocs may need this
-])
+sys.path.insert(0, os.path.abspath('..'))
 
 # -- Project information -----------------------------------------------------
 
