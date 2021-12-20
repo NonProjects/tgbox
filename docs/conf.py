2160f797687ff00a85eaa2dce860c0ffd03094d2
NotStatilko <valerastatilko@gmail.com>
2021-12-20 13:32:52 +0200

    Update conf.py to indev#0.2

--- docs/conf.py
+++ docs/conf.py
@@ -30,7 +30,7 @@ copyright = '2021, NonProjects'
 author = 'NonProjects'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1.6'
+release = '0.2'
 
 
 # -- General configuration ---------------------------------------------------
