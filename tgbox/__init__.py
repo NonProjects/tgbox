50cd036c2b63b80074f19a70aea0b7542e828346
NotStatilko <valerastatilko@gmail.com>
2021-12-21 23:09:39 +0200

    Add sync(*) function to call coros

--- tgbox/__init__.py
+++ tgbox/__init__.py
@@ -1,6 +1,9 @@
 from asyncio import (
     new_event_loop, set_event_loop
 )
+from typing import Coroutine
+
+
 __all__ = [
     'api',
     'constants',
@@ -14,3 +17,11 @@ __all__ = [
 # Define and set global event loop
 loop = new_event_loop()
 set_event_loop(loop)
+
+
+def sync(coroutine: Coroutine):
+    """
+    Will call asynchronous function
+    in ``tgbox.loop`` and return result.
+    """
+    return loop.run_until_complete(coroutine)
