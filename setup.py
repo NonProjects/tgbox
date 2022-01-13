2fefe4e97159a2e01066dc9832d947557cf89d13
NotStatilko <thenonproton@protonmail.com>
2022-01-13 20:53:25 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -46,7 +46,6 @@ setup(
         'Topic :: System :: Archiving :: Backup',
         'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9'
