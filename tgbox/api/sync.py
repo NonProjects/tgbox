ad478d2d9e5274600d3ab5ba088accbdbc3162f8
NotStatilko <thenonproton@protonmail.com>
2024-04-14 11:59:46 +0300

    Update abstract contents in __init__.py after sync
    Plus: logger in tgbox.api.sync
    Plus: '__all__ = []' in tgbox.api.sync

--- tgbox/api/sync.py
+++ tgbox/api/sync.py
@@ -17,6 +17,8 @@ scripts and the runtime overhead is relatively low.<---
 All you should do is to firstly import this module, then anything you want.
 """
 
+import logging
+
 from . import local
 from . import remote
 from . import abstract
@@ -27,6 +29,10 @@ from .local import (
 )
 from .abstract import Box, BoxFile
 from .utils import TelegramClient, syncify
+
+__all__ = []
+
+logger = logging.getLogger(__name__)
 
 syncify(
     Box, BoxFile, TelegramClient,
@@ -41,3 +47,32 @@ syncify(
 # it additionally later. This flags will help us.
 Box._needs_syncify = True # pylint: disable=W0212
 BoxFile._needs_syncify = True # pylint: disable=W0212
+
+# We import classes and functions from the abstract.py module
+# in __init__.py (.) so they can be accessed via the
+# from 'tgbox.api import Box' (e.g). As this import comes
+# before the user imports 'tgbox.api.sync', __init__.py
+# caches the Async versions of this functions. So, for
+# example, after 'import tgbox.api.sync' the functions
+# or classes in __init__.py (get_box, Box, etc) will
+# stay the same, but in abstract.py they will be synced.
+#
+# from tgbox.api import get_box <-- Will stay Async
+# from tgbox.api.abstract import get_box <-- Will become Sync
+#
+# This is a strange behaviour and below we fix it by
+# updating __dict__ of the __init__.py with synced
+# versions of classes/functions. A bit quirky way,
+# but will resolve our issue.
+from .abstract import (
+    __dict__ as abstract__dict__,
+    __all__ as abstract__all__
+)
+from . import __dict__ as root__dict__
+
+for k,v in root__dict__.items():
+    # We update only things that presented in both
+    # modules AND in tgbox.api.abstract.__all__
+    if k in abstract__dict__ and k in abstract__all__:
+        logger.debug('__init__.%s was updated!' % k)
+        root__dict__[k] = abstract__dict__[k]
